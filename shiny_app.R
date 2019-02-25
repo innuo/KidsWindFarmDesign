5622f57971a7071078e92e804dd7bcb35e4a0143
Veeramachaneni <harshav@spwrk098.windlogics.local>
2019-02-25 08:19:15 -0600

    minor ui change

--- shiny_app.R
+++ shiny_app.R
@@ -2,13 +2,7 @@ library(shiny)
 library(DT)
 source("wind_array.R")
 
-cur_x <- 1
-cur_y <- 1
-
-turbine_array <- data.frame(x = numeric(0), y=numeric(0), wind.speed=numeric(0))
 init_wind_map <- make_initial_wind_map(5)
-cur_wind_map <- init_wind_map
-
 
 ui <- fluidPage(
   # Some custom CSS for a smaller font for preformatted text
@@ -19,12 +13,12 @@ ui <- fluidPage(
                     }
                     "))
     ),
-  
+
   fluidRow(
-    column(width = 6,
+    column(width = 3,
            # In a plotOutput, passing values for click, dblclick, hover, or brush
            # will enable those interactions.
-           plotOutput("plot1", height = "500px", width = "200%",
+           plotOutput("plot1", height = "600px", width = "250%",
                       # Equivalent to: click = clickOpts(id = "plot_click")
                       click = clickOpts(id = "plot_click"),
                       dblclick = dblclickOpts(
@@ -35,15 +29,15 @@ ui <- fluidPage(
                         delay=50
                       )
            )
+    ),
+    column(width = 4, offset = 4,
+           DT::dataTableOutput('table')
     )
   ),
-  
+
   fluidRow(
     column(width = 3,
            verbatimTextOutput("hover_info")
-    ),
-    column(width = 5,
-           dataTableOutput('table')
     ),
     column(width = 3,
            verbatimTextOutput("click_info")
@@ -52,43 +46,54 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  cur_x <<- 1
+  cur_y <<- 1
+
+  turbine_array <<- data.frame(x = numeric(0), y=numeric(0), wind.speed=numeric(0))
+  cur_wind_map <<- init_wind_map
+
   output$plot1 <- renderPlot({
      plot_wind_map(cur_wind_map)
   })
-  
+
   observeEvent(input$plot_click, {
     cur_x <<-  round(input$plot_hover$x * domain_dims[1])
     cur_y <<- round(input$plot_hover$y * domain_dims[2])
-    turbine_array <<- rbind(turbine_array, 
+
+    cur_x <<- max(min(cur_x, domain_dims[1]), 1)
+    cur_y <<- max(min(cur_y, domain_dims[2]), 1)
+
+
+    turbine_array <<- rbind(turbine_array,
                             data.frame(x = cur_x, y=cur_y, ws=round(cur_wind_map[cur_y, cur_x], 1)))
     tw = turbine_conditional_wind_map(c(cur_y, cur_x), init_wind_map)
     cur_wind_map <<- pmin(tw, cur_wind_map)
     turbine_array$wind.speed <- round(cur_wind_map[cbind(turbine_array$y, turbine_array$x)], 1)
-    output$table <- renderDataTable(DT::datatable(turbine_array, 
+    output$table <- renderDataTable(DT::datatable(turbine_array,
                                                   options = list(searching = FALSE, paging = FALSE)))
-    
+
     output$plot1 <- renderPlot({
       plot_wind_map(cur_wind_map)
     })
   })
-  
+
   output$hover_info <- renderPrint({
     cur_x <-  round(input$plot_hover$x * domain_dims[1])
     cur_y <- round(input$plot_hover$y * domain_dims[2])
-    
-    cur_x <- max(min(cur_x, domain_dims[1]), 0)
-    cur_y <- max(min(cur_y, domain_dims[2]), 0)
+
+    cur_x <- max(min(cur_x, domain_dims[1]), 1)
+    cur_y <- max(min(cur_y, domain_dims[2]), 1)
     cat("Location and wind speed\n")
     cat(paste0("x = ", cur_x, "\ny = ", cur_y,
                "\nWind speed = ", round(cur_wind_map[cur_y, cur_x], 1)))
   })
-  
+
   output$dblclick_info <- renderPrint({
     cat("input$plot_dblclick:\n")
     str(input$plot_dblclick)
   })
-  
-  
+
+
 }
 
 
